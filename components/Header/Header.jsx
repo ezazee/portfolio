4debe52fcd7c4d107b2308e77273dc1d9ec583f0
ezazee <yukiirima123@gmail.com>
2022-08-23 10:55:36 +0700

    first commit

---
+++ components/Header/Header.jsx
@@ -0,0 +1,100 @@
+import React, { useRef, useEffect } from "react";
+
+import { Container } from "reactstrap";
+import classes from "./header.module.css";
+import Link from "next/link";
+
+const NAV__LINK = [
+  {
+    path: "/",
+    display: "Home",
+  },
+  {
+    path: "#about",
+    display: "Tentang Diriku",
+  },
+  {
+    path: "#services",
+    display: "Layanan",
+  },
+  {
+    path: "#portfolio",
+    display: "Portfolio",
+  },
+  {
+    path: "#contact",
+    display: "Kontak",
+  },
+];
+
+const Header = () => {
+  const headerRef = useRef(null);
+
+  const menuRef = useRef(null);
+
+  const headerFunc = () => {
+    if (
+      document.body.scrollTop > 80 ||
+      document.documentElement.scrollTop > 80
+    ) {
+      headerRef.current.classList.add(`${classes.header__shrink}`);
+    } else {
+      headerRef.current.classList.remove(`${classes.header__shrink}`);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", headerFunc);
+
+    return () => window.removeEventListener("scroll", headerFunc);
+  }, []);
+
+  const toggleMenu = () =>
+    menuRef.current.classList.toggle(`${classes.menu__active}`);
+
+  return (
+    <header className={`${classes.header}`} ref={headerRef}>
+      <Container>
+        <div className={`${classes.nav__wrapper}`}>
+          {/* ======== navigation logo ======== */}
+          <div className={`${classes.logo}`}>
+            <h1>
+              <span>M.</span>Reza
+            </h1>
+          </div>
+
+          {/* ========= nav menu =========== */}
+          <div
+            className={`${classes.navigation}`}
+            ref={menuRef}
+            onClick={toggleMenu}
+          >
+            <div className={`${classes.nav__menu}`}>
+              {NAV__LINK.map((item, index) => (
+                <Link href={item.path} key={index}>
+                  {item.display}
+                </Link>
+              ))}
+
+              <div className={`${classes.nav__right}`}>
+                <p className=" d-flex align-items-center gap-2 mb-0">
+                  {" "}
+                  <a target="_blank" href="https://wa.me/+6281313711180">
+                    {" "}
+                    <i className="ri-whatsapp-line"></i> WhatsApp Me{" "}
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <span className={`${classes.mobile__menu}`}>
+            <i className="ri-menu-line" onClick={toggleMenu}></i>
+          </span>
+        </div>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
