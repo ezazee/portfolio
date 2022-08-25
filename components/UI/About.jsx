7ac1664be55d2ba589cd1f039ad060689639bce6
ezazee <yukiirima123@gmail.com>
2022-08-25 11:01:32 +0700

    sdsdsd

--- components/UI/About.jsx
+++ components/UI/About.jsx
@@ -4,10 +4,10 @@ import Link from "next/link";
 import SectionSubtitle from "./SectionSubtitle";
 import Image from "next/image";
 
-import img01 from "../../public/images/eza.jpg";
+import img01 from "../../public/images/portfolio-02.jpg.jpg";
 import img02 from "../../public/images/eza.jpg";
-import img03 from "../../public/images/eza.jpg";
-import img04 from "../../public/images/eza.jpg";
+import img03 from "../../public/images/img-02.jpg";
+import img04 from "../../public/images/atas.jpg";
 
 import classes from "../../styles/about.module.css";
 
